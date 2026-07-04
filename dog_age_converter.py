2e413e52ec24d474f24b0e5f3be157107550fbf2
courtney-swift <courtneydezman@gmail.com>
2026-07-05 02:12:53 +0900

    Assignment 1 comments

--- dog_age_converter.py
+++ dog_age_converter.py
@@ -1,3 +1,8 @@
+#This line of code is for the input to be a number which is why int which is integer is used.
 dogs_age= int (input("What is your dog`s age in human years?"))
+
+# Here I am coding the equation to take whatever number entered and multiple it by 7
 dogs_age_in_dog_years = dogs_age * 7
+
+# Here im coding th f string to display the information that has be concatenated to display the answer. 
 print(f"Your dog is {dogs_age_in_dog_years} in dog years")
